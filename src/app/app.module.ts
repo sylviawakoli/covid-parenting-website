884d5e72583c29b649355dd32628a5411e5246e6
maxwellfundi <fundimaxwell@gmail.com>
2020-06-15 15:55:56 +0300

    Adding contributors component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContributorsComponent } from './contributors/contributors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    ContributorsComponent
   ],
   imports: [
     BrowserModule,
