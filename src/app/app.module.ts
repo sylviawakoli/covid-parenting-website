bacbc2d85b006e9ed063bbc79fe74ddee29ea514
Michael Clapham <michaeljclapham@gmail.com>
2020-06-19 00:30:47 +0100

    TG-287 Added menu bar that responds to screen size and uses animated dropdown menu when screen is small.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContributorsComponent } from './contributors/contributors.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContributorsComponent } from './contributors/contributors.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
