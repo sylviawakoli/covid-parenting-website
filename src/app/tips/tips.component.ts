ab6e4e19c905956926ea7110c6f8eb4d18151ea3
Michael Clapham <michaeljclapham@gmail.com>
2020-06-23 13:25:46 +0100

    Added language dropdown.

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -9,6 +9,8 @@ export type LanguageCSVRow = {
   title: string
 };
 
+type Language = { name: string, code: string };
+
 @Component({
   selector: 'app-tips',
   templateUrl: './tips.component.html',
@@ -16,21 +18,38 @@ export type LanguageCSVRow = {
 })
 export class TipsComponent implements OnInit {
 
-  langCode = "af";
+  allLanguages: Language[] = [
+    { name: "Afrikaans", code: "af" },
+    { name: "English", code: "en" }
+  ];
+
+  currentLanguage: Language = {
+    code: "en",
+    name: "English"
+  };
 
   tipSheets: { title: string, thumnailSrc: string; pdfSrc: string }[] = [];
 
   constructor(private http: HttpClient, private spreadsheetService: SpreadsheetService) {
+    this.fetchTipSheets();
+  }
+
+  fetchTipSheets(){
     this.spreadsheetService.getCSVObjects("/assets/tip_sheets/tipSheetNames.csv").subscribe((rows: LanguageCSVRow[]) => {
-      let currentLanguageRows = rows.filter((row) => row.languageCode === this.langCode);
+      let currentLanguageRows = rows.filter((row) => row.languageCode === this.currentLanguage.code);
       this.tipSheets = currentLanguageRows.map((row) => {
         return {
           title: row.title,
           thumnailSrc: `/assets/images/tip_sheet_thumbnails/${row.tipSheetNumber}.webp`,
-          pdfSrc: `/assets/tip_sheets/${this.langCode}/${row.tipSheetNumber}.pdf`
+          pdfSrc: `/assets/tip_sheets/${this.currentLanguage.code}/${row.tipSheetNumber}.pdf`
         };
       })
     });
+  }
+
+  changeLanguage(language: Language) {
+    this.currentLanguage = language;
+    this.fetchTipSheets();
   }
 
   ngOnInit(): void {
