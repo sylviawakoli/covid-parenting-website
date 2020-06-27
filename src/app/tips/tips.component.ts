128c362279a3944a04ad8ba017c04af879a783c5
Michael Clapham <michaeljclapham@gmail.com>
2020-06-28 00:14:21 +0100

    Only load tip sheets once and use csv to populate language dropdown.

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SpreadsheetService } from '../shared/services/spreadsheet.service';
+import { Observable } from 'rxjs';
 
 export type LanguageCSVRow = {
   languageCode: string,
@@ -11,6 +12,8 @@ export type LanguageCSVRow = {
 
 type Language = { name: string, code: string };
 
+type TipSheet = { title: string, thumnailSrc: string; pdfSrc: string };
+
 @Component({
   selector: 'app-tips',
   templateUrl: './tips.component.html',
@@ -18,38 +21,48 @@ type Language = { name: string, code: string };
 })
 export class TipsComponent implements OnInit {
 
-  allLanguages: Language[] = [
-    { name: "Afrikaans", code: "af" },
-    { name: "English", code: "en" }
-  ];
+  allLanguages: Language[] = [];
 
   currentLanguage: Language = {
     code: "en",
     name: "English"
   };
 
-  tipSheets: { title: string, thumnailSrc: string; pdfSrc: string }[] = [];
+  tipSheetsByLanguage: { [langCode: string]: TipSheet[] } = {};
+
+  tipSheets: TipSheet[] = [];
 
   constructor(private http: HttpClient, private spreadsheetService: SpreadsheetService) {
     this.fetchTipSheets();
   }
 
-  fetchTipSheets(){
-    this.spreadsheetService.getCSVObjects("/assets/tip_sheets/tipSheetNames.csv").subscribe((rows: LanguageCSVRow[]) => {
-      let currentLanguageRows = rows.filter((row) => row.languageCode === this.currentLanguage.code);
-      this.tipSheets = currentLanguageRows.map((row) => {
-        return {
-          title: row.title,
-          thumnailSrc: `/assets/images/tip_sheet_thumbnails/${row.tipSheetNumber}.webp`,
-          pdfSrc: `/assets/tip_sheets/${this.currentLanguage.code}/${row.tipSheetNumber}.pdf`
-        };
-      })
+  fetchTipSheets() {
+    this.spreadsheetService.getCSVObjects("/assets/tip_sheets/tipSheetNames.csv")
+      .subscribe((rows: LanguageCSVRow[]) => {
+      rows.forEach((row) => {
+        let langCode = row.languageCode ? row.languageCode.toLowerCase().trim() : null;
+        if (langCode !== null) {
+          if (!this.tipSheetsByLanguage[langCode]) {
+            this.tipSheetsByLanguage[langCode] = [];
+            this.allLanguages.push({
+              code: langCode,
+              name: row.languageName
+            });
+          }
+          this.tipSheetsByLanguage[langCode].push({
+            title: row.title,
+            thumnailSrc: `/assets/images/tip_sheet_thumbnails/${row.tipSheetNumber}.webp`,
+            pdfSrc: `/assets/tip_sheets/${langCode}/${row.tipSheetNumber}.pdf`
+          });
+        }
+      });
+      this.tipSheets = this.tipSheetsByLanguage[this.currentLanguage.code];
     });
   }
 
   changeLanguage(language: Language) {
     this.currentLanguage = language;
-    this.fetchTipSheets();
+    this.tipSheets = this.tipSheetsByLanguage[this.currentLanguage.code];
   }
 
   ngOnInit(): void {
