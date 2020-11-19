3bd45a5114cda7925801af74d108043ab8689c59
patowhiz <conlooptechnologies@gmail.com>
2020-11-19 12:14:12 +0300

    banner and newsletter additions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { WworeachComponent } from './wworeach/wworeach.component';
 import { ImpactStoriesComponent } from './impact-stories/impact-stories.component';
 import { ShareYourStoryComponent } from './share-your-story/share-your-story.component';
 import { SuggestContentComponent } from './suggest-content/suggest-content.component';
+import { NewletterribbonComponent } from './newletterribbon/newletterribbon.component';
 
 @NgModule({
   declarations: [
@@ -73,7 +74,8 @@ import { SuggestContentComponent } from './suggest-content/suggest-content.compo
     WworeachComponent,
     ImpactStoriesComponent,
     ShareYourStoryComponent,
-    SuggestContentComponent
+    SuggestContentComponent,
+    NewletterribbonComponent
   ],
   imports: [
     BrowserModule,
