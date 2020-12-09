2ca1770d70a0682d1dd8cd48060181c9d13b78ec
patowhiz <conlooptechnologies@gmail.com>
2020-12-09 11:34:31 +0300

    impact stories changes and newsletter ribbon design changes

--- src/app/impact-stories/impact-stories.service.ts
+++ src/app/impact-stories/impact-stories.service.ts
@@ -7,6 +7,7 @@ export type ImpactSummary = {
   impactType: string;
   impactTitle: string;
   impactNumber: number;
+  impactPercentage: number;
   thumbnailSrc: string;
 };
 
@@ -14,6 +15,7 @@ export type ImpactSummaryCSVRow = {
   impactType: string;
   impactTitle: string;
   impactNumber: number;
+  impactPercentage: number;
 };
 
 @Injectable({
@@ -31,19 +33,21 @@ export class ImpactStoriesService {
           let arrImpactSummaries: ImpactSummary[] = [];
           let thumbnailSrc: string;
           rows.forEach((row) => {
-            //add row contents to the array
-            if (row.impactType.toUpperCase() === "REGION") {
-              thumbnailSrc = `assets/impacts/${row.impactTitle}.jpg`
-            } else {
-              thumbnailSrc= ""         
+            try {
+              //add row contents to the array
+              thumbnailSrc = row.impactType.toUpperCase() === "REGION"? `assets/impacts/${row.impactTitle}.jpg` : "";
+
+              arrImpactSummaries.push({
+                impactType: row.impactType,
+                impactTitle: row.impactTitle,
+                impactNumber: row.impactNumber,
+                impactPercentage: row.impactPercentage,
+                thumbnailSrc: thumbnailSrc
+              });
+            } catch (ex) {
+
             }
-            
-            arrImpactSummaries.push({
-              impactType: row.impactType,
-              impactTitle: row.impactTitle,
-              impactNumber: row.impactNumber,
-              thumbnailSrc: thumbnailSrc
-            });
+
 
           });
 
