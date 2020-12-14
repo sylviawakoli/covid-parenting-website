0684875ba2f56b6084ab25c934f0e0c234a7b1e1
patowhiz <conlooptechnologies@gmail.com>
2020-12-14 16:18:14 +0300

    aded real impact stories

--- src/app/impact/impact.component.ts
+++ src/app/impact/impact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImpactService, ImpactSummary } from './impact.service';
 
 @Component({
   selector: 'app-impact',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpactComponent implements OnInit {
 
-  constructor() { }
+  public strCurrentDate: string="";
+  public arrRegionSummaries: ImpactSummary[] = [];
+  public arrDisseminationSummaries: ImpactSummary[] = [];
+  public totalRegionSummary: number=0;
+  public totalDisseminationSummary: number=0;
+
+  constructor(private impactStoriesService: ImpactService) {
+    this.strCurrentDate = new Date().toLocaleDateString();
+
+    impactStoriesService.fetchImpactSummaries().subscribe((impactsummaries) => {
+      impactsummaries.forEach((impactsummary) => {
+
+        if (impactsummary.impactType.toUpperCase() === "REGION") {
+          this.arrRegionSummaries.push(impactsummary)
+          this.totalRegionSummary = this.totalRegionSummary + impactsummary.impactNumber;
+        } else {
+          this.arrDisseminationSummaries.push(impactsummary)
+          this.totalDisseminationSummary = this.totalDisseminationSummary + impactsummary.impactNumber;
+        }
+
+      });
+
+    });
+  }
 
   ngOnInit(): void {
   }
