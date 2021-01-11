fdef81ceb5372c0a0fc859dbaf07138fe67a3610
maxwellfundi <fundimaxwell@gmail.com>
2021-01-11 16:18:32 +0300

    Adding various small changes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -29,7 +29,7 @@ const MENU_ITEMS: MenuItem[] = [
       { name: "Tips For Caseworkers", href: "/caseworkers" },
       { name: "Public Service Announcements", href: "/psa" },
       { name: "Digital Parenting", href: "/digitalparenting" },
-      { name: "Audio-Visual", href: "/audiovisuals" },
+      { name: "Audiovisual Resources", href: "/audiovisuals" },
       { name: "Faith-Based Resources", href: "/faithleaders" },
       { name: "Social Media", href: "/socialmedia" },
       /**   {
