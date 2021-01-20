7c5043a8c776335b04933ad7f6b7f7c43fa72015
patowhiz <conlooptechnologies@gmail.com>
2021-01-20 20:42:18 +0300

    animating impact numbers

--- src/app/impact/impact.component.ts
+++ src/app/impact/impact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ImpactService, ImpactSummary } from './impact.service';
 
 @Component({
@@ -8,18 +8,17 @@ import { ImpactService, ImpactSummary } from './impact.service';
 })
 export class ImpactComponent implements OnInit {
 
-  public strCurrentDate: string="";
+  public strCurrentDate: string = "";
   public arrRegionSummaries: ImpactSummary[] = [];
   public arrDisseminationSummaries: ImpactSummary[] = [];
-  public totalRegionSummary: number=0;
-  public totalDisseminationSummary: number=0;
+  public totalRegionSummary: number = 0;
+  public totalDisseminationSummary: number = 0;
 
-  constructor(private impactStoriesService: ImpactService) {
+  constructor(private elem: ElementRef, private impactStoriesService: ImpactService) {
     this.strCurrentDate = new Date().toLocaleDateString();
 
     impactStoriesService.fetchImpactSummaries().subscribe((impactsummaries) => {
       impactsummaries.forEach((impactsummary) => {
-
         if (impactsummary.impactType.toUpperCase() === "REGION") {
           this.arrRegionSummaries.push(impactsummary)
           this.totalRegionSummary = this.totalRegionSummary + impactsummary.impactNumber;
@@ -27,13 +26,53 @@ export class ImpactComponent implements OnInit {
           this.arrDisseminationSummaries.push(impactsummary)
           this.totalDisseminationSummary = this.totalDisseminationSummary + impactsummary.impactNumber;
         }
-
       });
 
+
+      this.setAndAnimateNumberCounts(this.elem.nativeElement.querySelectorAll('.total-dissemination'), this.totalDisseminationSummary);
+      this.setAndAnimateNumberCounts(this.elem.nativeElement.querySelectorAll('.total-region'), this.totalRegionSummary);
+
     });
+
+
   }
 
   ngOnInit(): void {
   }
+
+
+  ngAfterViewInit() {
+  }//end method
+
+ 
+
+  private setAndAnimateNumberCounts(htmlTag: any, innerHtmlValue: number){
+    for (let i: number = 0; i < htmlTag.length; i++) {
+        htmlTag[i].innerHTML = innerHtmlValue;   
+    }
+    this.animateTags(htmlTag);
+  }
+
+  private animateTags(htmlTag: any): void{
+    let strNum: string;
+    for (let i: number = 0; i < htmlTag.length; i++) {
+      strNum = htmlTag[i].innerHTML; 
+      this.animateValue(htmlTag[i], 0, parseFloat(strNum.replace(/,/g, '')), 5000);
+    }
+  }
+
+  private animateValue(obj, start: number, end: number, duration: number): void {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start).toLocaleString();
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }//end method
+
 
 }
