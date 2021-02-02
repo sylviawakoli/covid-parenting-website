a769fd6dc6b611e0f02782030ec3453eb224da8a
Mike Nsubuga <nsubugamike021@gmail.com>
2021-02-02 15:58:03 +0300

    added information to markers on impact page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -19,17 +19,28 @@ export class MapComponent implements OnInit {
  
 
   private initMap() {
-    this.map = L.map('map').setView([51.505, 0], 2);
+    this.map = L.map('map',{
+      zoomDelta:0.25,
+      zoomSnap:0.5,
+      minZoom: 2,
+      maxZoom:2,
+      dragging: false
+    }).setView([0.0, 0], 2);
 
-    const tiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
       maxZoom: 20,
       attribution:'Map data © OpenStreetMap contributors'
     });
-  L.marker([51.5, -0.09]).addTo(this.map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+
+    const myIcon = L.icon({
+      iconUrl:'assets/icon/favicon.png',
+      iconSize:[30, 30]
+    })
+  L.marker([2.25, 20.30], {icon: myIcon}).addTo(this.map)
+    .bindPopup('What this initiative has done to my family is so profound. I am able to appreciate the world of children, and I think I am learning good parenting. It has also challenged me to look at this crazy time I have found myself i through the positive lens of hope.".<br> - Parent in Malawi from Without Orphans')
     .openPopup();
 
-  L.marker([45, 23.10]).addTo(this.map)
+  L.marker([45, 23.10], {icon: myIcon}).addTo(this.map)
     .bindPopup('Easily customizable.')
     .openPopup();
     tiles.addTo(this.map);
