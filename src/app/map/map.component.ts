11faf5976b05ae6ceaa22f561451db19e7cdfc90
Mike Nsubuga <nsubugamike021@gmail.com>
2021-02-03 11:01:40 +0300

    combined Malawi quotes and reduced size of marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -34,14 +34,10 @@ export class MapComponent implements OnInit {
 
     const myIcon = L.icon({
       iconUrl:'assets/icon/favicon.png',
-      iconSize:[30, 30]
+      iconSize:[18, 18]
     })
   L.marker([-3.25, 33.30], {icon: myIcon}).addTo(this.map)
-    .bindPopup('What this initiative has done to my family is so profound. I am able to appreciate the world of children, and I think I am learning good parenting. It has also challenged me to look at this crazy time I have found myself i through the positive lens of hope.".<br> - Parent in Malawi from Without Orphans')
-    .openPopup();
-
-    L.marker([-13.10, 35.10], {icon: myIcon}).addTo(this.map)
-    .bindPopup('We understand that it is very hard for parents to adjust to the new norm, as they will have their children at home more than they are used to due to schools being closed. [The COVID-19 parenting resources] not only bring relief as we parent, but also challenge parenting in general, for some of us have failed miserably to parent well. It will leave a lasting impact and is amazing we have learnt this through a church in our community. <br> - Parent from Malawi ')
+    .bindPopup('What this initiative has done to my family is so profound. I am able to appreciate the world of children, and I think I am learning good parenting. It has also challenged me to look at this crazy time I have found myself i through the positive lens of hope.".<br> - Parent in Malawi from Without Orphans <br><br> We understand that it is very hard for parents to adjust to the new norm, as they will have their children at home more than they are used to due to schools being closed. [The COVID-19 parenting resources] not only bring relief as we parent, but also challenge parenting in general, for some of us have failed miserably to parent well. It will leave a lasting impact and is amazing we have learnt this through a church in our community. <br> - Parent from Malawi <br><br> Thanks to parenting tips materials, family relationships are being enhanced.Most of our 23 local church partners have testified to how parents have been awakened to what should be natural and fun in families. Thriving social life relationships in most families have, unfortunately, been non-existent; and thanks to Covid19 as it has simply exposed this fact..<br> - World Without Orphans, Malawi <br><br> If I have found these (tips) helpful, for sure they should be for all parents and caregivers in our village. They are also applicable to people of all faiths, and I will highly recommend to the village head so we could advocate across our community. They are timely.<br> - Counsellor to village Headman Mphamba in Malawi')
     .openPopup();
 
     L.marker([40, 139.10], {icon: myIcon}).addTo(this.map)
@@ -52,17 +48,11 @@ export class MapComponent implements OnInit {
     .bindPopup('The messages are important and guiding especially this time when adolescents are at home. <br> - Catholic Relief Services (CRS), Kenya ')
     .openPopup();
 
-    L.marker([-6.25, 34.30], {icon: myIcon}).addTo(this.map)
-    .bindPopup('Thanks to parenting tips materials, family relationships are being enhanced.Most of our 23 local church partners have testified to how parents have been awakened to what should be natural and fun in families. Thriving social life relationships in most families have, unfortunately, been non-existent; and thanks to Covid19 as it has simply exposed this fact..<br> - World Without Orphans, Malawi')
-    .openPopup();
-
+  
     L.marker([47, 10.10], {icon: myIcon}).addTo(this.map)
     .bindPopup('The children of the world have an urgent need for safe havens right now, in both a spiritual and physical sense. Spiritual support - through this kind of creative engagement designed especially for children -can give a renewed sense of hope to families and churches alike.<br> - WCC Interim General Secretary Rev. Prof. Dr Ioan Sauca about the Church Leaders Pack ')
     .openPopup();
 
-    L.marker([-1.25, 34.30], {icon: myIcon}).addTo(this.map)
-    .bindPopup('If I have found these (tips) helpful, for sure they should be for all parents and caregivers in our village. They are also applicable to people of all faiths, and I will highly recommend to the village head so we could advocate across our community. They are timely.<br> - Counsellor to village Headman Mphamba in Malawi')
-    .openPopup();
 
 
     tiles.addTo(this.map);
